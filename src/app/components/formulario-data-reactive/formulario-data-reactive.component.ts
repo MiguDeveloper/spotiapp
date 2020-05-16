51d18f669e4d82654f491602a9831a3551e0ffed
MiguDeveloper <miguel.peru.seo@gmail.com>
2020-05-16 00:15:16 -0500

    Formularios reactivos campos dinamicos formControlName formGroupName formArray

--- src/app/components/formulario-data-reactive/formulario-data-reactive.component.ts
+++ src/app/components/formulario-data-reactive/formulario-data-reactive.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {FormArray, FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-formulario-data-reactive',
@@ -15,47 +15,87 @@ export class FormularioDataReactiveComponent implements OnInit {
 
   ngOnInit() {
     this.iniciarFormulario();
+    this.cargarDataEnFormulario();
   }
 
-  get nombreNoValido() {
-    return this.formReactive.get('nombre').invalid && this.formReactive.get('nombre').touched;
+  get pasatiempos() {
+    return this.formReactive.get('pasatiempos') as FormArray;
   }
 
-  get nombreValido() {
-    return this.formReactive.get('nombre').valid && this.formReactive.get('nombre').touched;
+  formControlNoValido(control: string): boolean {
+    return this.formReactive.get(control).invalid && this.formReactive.get(control).touched;
   }
 
-  get apellidoNoValido() {
-    return this.formReactive.get('apellido').invalid && this.formReactive.get('apellido').touched;
+  formControlValido(control: string): boolean {
+    return this.formReactive.get(control).valid && this.formReactive.get(control).touched;
   }
 
-  get apellidoValido() {
-    return this.formReactive.get('apellido').valid && this.formReactive.get('apellido').touched;
+  formGroupNoValido(objeto: string): boolean {
+    return this.formReactive.get(objeto).invalid && this.formReactive.get(objeto).touched;
   }
 
-  get correoNoValido() {
-    return this.formReactive.get('correo').invalid && this.formReactive.get('correo').touched;
-  }
-
-  get correoValido() {
-    return this.formReactive.get('correo').valid && this.formReactive.get('correo').touched;
+  formGroupValido(objeto: string): boolean {
+    return this.formReactive.get(objeto).valid && this.formReactive.get(objeto).touched;
   }
 
   iniciarFormulario() {
     this.formReactive = this.fb.group({
       nombre: ['', [Validators.required, Validators.minLength(5)]],
       apellido: ['', Validators.required],
-      correo: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,3}$')]]
+      correo: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,3}$')]],
+      direccion: this.fb.group({
+        distrito: ['', Validators.required],
+        ciudad: ['', Validators.required]
+      }),
+      pasatiempos: this.fb.array([])
     });
+  }
+
+  /*
+   * Recordar que cuando hacemos el setValue({}) no puede faltar ningun campo con respecto al formulario
+   * sino marcara error. Para evitar ese error es mejor usar el reset para setear el formulario a la
+   * hora de cargar o iniciar el formulario
+   */
+  cargarDataEnFormulario() {
+    // this.formReactive.setValue({
+    this.formReactive.reset({
+      nombre: 'Miguel',
+      apellido: 'Chinchay',
+      correo: 'miguel.peru@gmail.com',
+      direccion: {
+        distrito: 'Otawa',
+        ciudad: 'Ontario'
+      }
+    });
+
+    ['Desarrollar', 'Futbol'].forEach(pasatiempo => this.pasatiempos.push(this.fb.control(pasatiempo)));
+  }
+
+  agregarPasatiempo() {
+    this.pasatiempos.push(this.fb.control('', Validators.required));
+  }
+
+  borrarPasatiempo(i: number) {
+    this.pasatiempos.removeAt(i);
   }
 
   submitForm() {
     console.log(this.formReactive);
     if (this.formReactive.invalid) {
       Object.values(this.formReactive.controls).forEach(control => {
-        control.markAsTouched();
+        if (control instanceof FormGroup) {
+          Object.values(control.controls).forEach(subControl => {
+            subControl.markAsTouched();
+          });
+        } else {
+          control.markAsTouched();
+        }
       });
     }
+    /* resteamos el formulario
+    this.formReactive.reset({
+      nombre: 'sin nombre'
+    });*/
   }
 
 }
